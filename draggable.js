0db267b6cf82cba20301a8f72ea9a7c875bad512
YmL <72396404+NotYmL@users.noreply.github.com>
2022-07-12 20:29:58 +0200

    Create draggable.js

---
+++ draggable.js
@@ -0,0 +1,25 @@
+async function draggable(grabP, move){
+    let lastPosX = null;
+    let lastPosY = null;
+
+    let op = document.body.getElementsByTagName("*");
+    for(let i = 0; i < op.length; i++) {
+        Object.assign(op[i].style, {userSelect: "none"});
+    }
+    
+    move.style.position = "absolute";
+    
+    grabP.addEventListener("mousemove", (e) => {
+        if(e['buttons']==1) {
+            
+            move.style.left = `${move.offsetLeft - (lastPosX - e.clientX)}px`;
+            move.style.top = `${move.offsetTop  - (lastPosY - e.clientY)}px`;
+    
+            lastPosY=e.clientY;
+            lastPosX=e.clientX;
+        } else {
+            lastPosX=e.clientX; 
+            lastPosY=e.clientY;
+        }
+    })
+}
